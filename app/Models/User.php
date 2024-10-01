140c028140ea954e6e36a7ed206c425961606d12
Ibrahim Ismail <ibrahim.ismail@miswag.com>
2024-10-02 01:28:24 +0300

    allow panel access

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gank.com');
+        return true;
     }
 
     /**
